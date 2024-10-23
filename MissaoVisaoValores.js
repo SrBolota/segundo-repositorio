40116f0f6d370a8f4bd150ce60d0b34a5daa50bd
SrBolota <138731554+SrBolota@users.noreply.github.com>
2024-10-22 22:02:17 -0300

    Aula 22/10

---
+++ MissaoVisaoValores.js
@@ -0,0 +1,19 @@
+criaCartao (
+   'Missão' ,
+   'Saiba qual é a missão de nossa empresa!' ,
+   'Nossa missãao é avaliar e concertar dispositivos para diminuir a troca sem necesidade' ,
+)
+
+criaCartao (
+   'Visão' ,
+   'Saiba qual é a nossa vissão para o futuro da empresa !' ,
+   'Esperamos conseguir no futuro,prolongar a vida útil de todos os dipositivos' ,
+
+)
+
+criaCartao (
+   'Valores' ,
+   'Saiba quais são os nossos principais valores!' ,
+   'Respeito,Trabalho em equipe,Inovação e Foco no cliente' ,
+
+)
