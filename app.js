6305a5663be1a41ce0c4b9dfd46b74bba26a8384
SrBolota <138731554+SrBolota@users.noreply.github.com>
2024-11-05 21:40:05 -0300

    Aula 05/11

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 function criaCartao (item, pergunta, resposta) {
-    console.log(item, pergunta, resposta)
+    //console.log(item, pergunta, resposta)
+
+
+    let flshcard = documet.getElementById('flashcard');
+    let cartao = document.createElement('article');
+
+    cartao.className = 'cartao';
+
+    cartao.innerHTML = `
+          <div class="cartao__conteudo">
+
+                    <h3>Missão</h3>
+
+                    
+                    <div class="cartao__conteudo__pergunta">
+                        </p>
+                            Saiba qual é a missao de nossa empresa!
+                        <p>
+                            
+                    </div>
+
+                    <div class="cartao__conteudo__resposta">
+                        <p>
+                            Avaliar e concertar dispositivos
+                            para diminuir a troca sem 
+                            necesidade
+                        </p>
+
+                    </div>
+                    
+                    
+                 </div>
+    `
+
+    flashcard.appendChield(cartao);
+
 }
+
